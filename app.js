05ca2993e3cac1ab95611903fd7d250332c20139
Bertin <38949731+sanobertin@users.noreply.github.com>
2018-11-02 12:04:02 +0200

    new changes

---
+++ app.js
@@ -0,0 +1,14 @@
+var fs= require('fs')
+var http= require('http')
+
+function onRequest(request, response){
+	response.writeHead(200, {'Content-Type':'text/html'});
+	fs.readFile('./UI/index.html', null, function(error, data){
+		if(error){response.writeHead(404); response.write('File not found');}
+		else {response.write(data)}
+		response.end();
+	})
+	response.end()
+}
+
+http.createServer(onRequest).listen(8000)
