955fff92994c40266d89889f4da7718efeeca792
sanobertin <sanomihigobertin@gmail.com>
2018-11-06 14:12:01 +0200

    OK

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-const fs = require('fs');
-const http = require('http');
+const express= require('express');
+const app= express();
+const path= require('path')
+ejs= require('ejs')
+app.use(express.static(__dirname + '/UI'));
+app.set('views', './UI') // specify the views directory
+app.set('view engine', 'ejs');
 
-console.log('OK');
-function onRequest(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.readFile('/UI/index.html', null, (error, data) => {
-    if (error) { response.writeHead(404); response.write('File not found'); } else { response.write(data); }
-    response.end();
-  });
-  response.end();
-}
+const profileroutes= require('./api/routes/profile.js');
 
-http.createServer(onRequest).listen(8000);
+app.get('/', (req, res) => {
+	res.render('index', {var1:'Bertin did it'});
+})
+
+/*/ app.get('/admin', (req, res) => {
+	res.sendFile(path.join(__dirname+'/UI/admin.html'));
+})
+
+app.get('/signin', (req, res) => {
+	res.sendFile(path.join(__dirname+'/UI/signin.html'));
+})
 
 
 
+app.use('/profile', profileroutes);
+app.use(express.static(path.join(__dirname + '/UI')));
 
+
+var parcels= ['parcel 1', 'parcel 1', 'parcel 2', 'parcel 3', 'parcel 4', 'parcel 5', 'parcel 6']
+
+app.get('/parcels', (req, res) => {
+	res.render(path.join(__dirname+'/UI/index.html'), {'var1': 'P1', 'var2':'P2'});
+}) */
+
+app.get('/signup', (req, res) => {
+	res.render('signup.html')
+})
+app.listen(3000);
+console.log('Server Is OK');
+
+
+module.exports= app;
---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const http = require('http');
+
+
+function onRequest(request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.readFile('./UI/index.html', null, (error, data) => {
+    if (error) { 
+    	response.writeHead(404); 
+    	response.write('File not found'); 
+    } 
+    else { response.write(data); }
+    response.end();
+  });
+}
+
+http.createServer(onRequest).listen(3000);
+console.log('OK');
+
+
+
+
+
