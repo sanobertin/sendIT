9a6110a01563f74e95523f8a30e5b76378de784e
sanobertin <sanomihigobertin@gmail.com>
2018-11-06 17:23:42 +0200

    Tues

--- app.js
+++ app.js
@@ -1,40 +1,51 @@
 const express= require('express');
 const app= express();
 const path= require('path')
-ejs= require('ejs')
+const fs= require('fs')
 app.use(express.static(__dirname + '/UI'));
 app.set('views', './UI') // specify the views directory
-app.set('view engine', 'ejs');
 
-const profileroutes= require('./api/routes/profile.js');
 
-app.get('/', (req, res) => {
-	res.render('index', {var1:'Bertin did it'});
+app.get('/api/v1/', (req, res) => {
+	res.render('index.html');
 })
 
-/*/ app.get('/admin', (req, res) => {
+ app.get('/api/v1//admin', (req, res) => {
 	res.sendFile(path.join(__dirname+'/UI/admin.html'));
 })
 
-app.get('/signin', (req, res) => {
+app.get('/api/v1/signin', (req, res) => {
 	res.sendFile(path.join(__dirname+'/UI/signin.html'));
 })
 
 
 
-app.use('/profile', profileroutes);
-app.use(express.static(path.join(__dirname + '/UI')));
-
-
-var parcels= ['parcel 1', 'parcel 1', 'parcel 2', 'parcel 3', 'parcel 4', 'parcel 5', 'parcel 6']
-
-app.get('/parcels', (req, res) => {
-	res.render(path.join(__dirname+'/UI/index.html'), {'var1': 'P1', 'var2':'P2'});
-}) */
-
-app.get('/signup', (req, res) => {
-	res.render('signup.html')
+app.get('/api/v1/:userID', (req, res, next) => {
+	res.status(200).json({
+		message: 'Handling Profile GET '
+	})
 })
+
+
+
+
+const users=[];
+
+users.push({name:'Admin', email:'Admin@sendit.dv', usernname:'admin', password:'admin1234', right:'admin'});
+
+app.post('/api/v1/signup', (req, res) => {
+	res.send('Good!')
+	var _name= req.name;
+	var mail= req.email;
+	var usern= req.usern;
+	var pass= req.pass;
+	var rights= 'normal';
+	fs.appendFile('users.txt', JSON.stringify({name: _name, email: mail, usernname: usern, password:pass, right:rights}), (err) => {
+		res.send('successfully registered')})
+	});
+
+	
+
 app.listen(3000);
 console.log('Server Is OK');
 
