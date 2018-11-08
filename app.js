0feb74d3d8c63a630c25515a4af000d3de482f3c
bazimya saphani <34911199+bazimya@users.noreply.github.com>
2018-11-08 20:00:25 +0200

    Thursday

--- app.js
+++ app.js
@@ -34,38 +34,76 @@ app.route('/api/v1/signin')
 //Data
 var admin= {name:'admin', email:'Admin@sendit.dv', usernname:'admin', password:'admin1234', right:'admin'}
 const users=[];
-users.push({name:'Admin', email:'Admin@sendit.dv', usernname:'admin', password:'admin1234', right:'admin'});
+users.push({name:'Admin', email:'Admin@sendit.dv', username:'admin', password:'admin1234', right:'admin'}); //admin acc
+
+users.push({name:'bertin', email:'bertin@sendit.dv', username:'bertin', password:'admin1234', right:'user'});
+users.push({name:'user2', email:'user2@sendit.dv', username:'user2', password:'admin1234', right:'user'});
 
 var parcels = [
-	{parcelID: 1, owner:users[0]['username'], parcelName:'Parcel 1'},
-	{parcelID: 2, owner:users[0]['username'], parcelName:'Parcel 2'},
-	{parcelID: 3, owner:users[0]['username'], parcelName:'Parcel 3'},
-	{parcelID: 4, owner:users[0]['username'], parcelName:'Parcel 4'},
-	{parcelID: 5, owner:users[0]['username'], parcelName:'Parcel 5'}
-]
+	{parcelID: 1, owner:users[1].username, parcelName:'Parcel 1', status:'Delivered'},
+	{parcelID: 2, owner:users[2].username, parcelName:'Parcel 2', status:'In transit'},
+	{parcelID: 3, owner:users[1].username, parcelName:'Parcel 3', status:'Canceled'},
+	{parcelID: 4, owner:users[2].username, parcelName:'Parcel 4', status:'Delivered'},
+	{parcelID: 5, owner:users[1].username, parcelName:'Parcel 5', status:'In transit'}
+];
+//End of data
 
 
-
-app.get('/api/v1/parcels', (req, res) => {
+app.get('/api/v1/parcels', (req, res) => { // working
 	res.status(200).send(parcels);
-})
+});
 
-app.get('/api/v1/parcels/:parcel_ID', (req, res) => {
+app.get('/api/v1/parcels/:parcel_ID', (req, res) => { // Working
 	parcel_ID= req.params.parcel_ID;
 	for (var a=0; a <parcels.length; a++){
 		if(parcel_ID==parcels[a].parcelID) {
-			res.status(200).res.send(parcels[a]);
+			console.log(toString(parcels[a].parcelID) + ' fetched!')
+			res.status(200).send(parcels[a]);
 			res.end;
+			break;
 		}
 		else {res.status(500).end()}
 	}
 	//res.status(200).res.send('OK')
-})
+});
 
-app.get('/api/v1/:username', (req, res) => {
-	res.send('You are Welcome '+ req.params.username)
-	//res.render('index.html', {username: req.params.username});
-})
+app.get('/api/v1/users/:userID', (req, res) => { // Working 
+	userID= req.params.userID;
+	for (var a=0; a < users.length; a++){
+		if(userID != users[a].username) { res.status(404).json({message: 'User doesnt exist'}); }
+		else if(userID==users[a].username){
+			console.log(toString(users[a].username) + ' fetched!!');
+			res.status(200).send(users[a]);
+			res.end();
+			break;
+		}
+		//else { res.status(404).json({message: 'user does not exist'})}
+	}
+
+
+});
+
+
+
+app.get('/api/v1/users/:userID/parcels', (req, res) => { // Working 
+	userID= req.params.userID;
+	for (var a=0; a < users.length; a++){
+		if(userID != users[a].username){ res.status(404).json({message: 'User doesnt exist'}); break;}
+		else if(userID==users[a].username){
+			console.log(toString(users[a].username) + ' fetched!!');
+			res.status(200).send(users[a]);
+			res.end();
+			break;
+		}
+		//else { res.status(404).json({message: 'user does not exist'}).end()}
+	}
+
+});
+
+app.put('/api/v1/parcels/:parcel_ID/cancel', (req, res) => { 
+	parcel_ID= req.params.parcel_ID;
+	
+});
 
  /*p.get('/api/v1/admin', (req, res) => {
 	res.sendFile(path.join(__dirname+'/UI/admin.html'));
