7c0d74059d3f99592dca1a48169b5ca297b965d1
Bertin <sanomihigobertin@gmail.com>
2018-11-26 20:59:35 +0200

    [Feature #162149696]

--- api/routes/parcels.js
+++ api/routes/parcels.js
@@ -3,25 +3,30 @@ const router = express.Router()
 const bodyparser = require('body-parser').json();
 const parcels= require('../models/db').parcels
 const users= require('../models/db').users
+const jwt= require('jsonwebtoken')
+require('dotenv').config()
+jwtkey= process.env.jwtkey;
 
 router.get('/api/v1/parcels', (req, res) => { // working *2
 	if(parcels===undefined){
 		res.status(404).json({message:'Not parcels exist'})
 	} else {
-  res.status(200).json(parcels); }
+		//token= jwt.sign({parcels}, jwtkey, { expiresIn: '1h' })
+  	res.status(200).json(parcels); }
 });
+const getAllParcelsQuery= 'SELECT * from parcels';
 
 router.post('/api/v1/parcels', bodyparser, (req, res) => { 
-  newp = req.body.parcel;
+  newp = JSON.parse(req.body.parcel);
   if (Object.keys(newp).length == 0) { // testing empty obj
     res.status(404).json({ message: 'Empty object!' });
   } else {
     parcels.push(newp);
     console.log('new parcel order Created');
-    //console.log(parcels[this.length -1]);
-    res.status(201).json({ message: 'Created' });
+    res.status(201).json({ message: 'Created', "new parcel":newp });
   }
 });
+let createNewParcelQuery=`INSERT INTO parcels () VALUES ()`; //not ending
 
 router.get('/api/v1/parcels/:parcel_ID', (req, res) => { // Working *2
   parcel_ID = parseInt(req.params.parcel_ID);
@@ -32,6 +37,7 @@ router.get('/api/v1/parcels/:parcel_ID', (req, res) => { // Working *2
   res.status(200).json(fetched_parcel)}  
  
 });
+let getSpecificParcelQuery= `SELECT * from parcels where id=${parcel_ID}`
 
 router.put('/api/v1/parcels/:parcel_ID/cancel', (req, res) => { // Working not as i need
 	parcel_ID = parseInt(req.params.parcel_ID);
@@ -44,7 +50,7 @@ router.put('/api/v1/parcels/:parcel_ID/cancel', (req, res) => { // Working not a
 		fp.status='Canceled'
 		res.status(200).json({message:"Successfully updated"});
 	}
-
+let updateSpecificParcelCancelQuery=`UPDATE parcels set status='canceled' where id=${parcel_ID}`
 	/*for (let a = 0; a < parcels.length; a++) {
 		if (parcel_ID == parcels[a].parcelID) {
 		  console.log('ID found!');
@@ -58,6 +64,18 @@ router.put('/api/v1/parcels/:parcel_ID/cancel', (req, res) => { // Working not a
 	}
 	console.log(parcels); */
 });
+router.put('/api/v1/parcels/:parcelID/destination', (req, res) =>{
+	//change the location of a specific parcel order
+})
+let updateSpecificDestinationQuery=`UPDATE parcels set destination=${newDestination} where id=${parcel_ID}`
+router.put('/api/v1/parcels/:parcelID/status', (req, res) =>{ //accessible by admins only
+	//change the status of a specific parcel order
+})
+let updateSpecificParcelStatusQuery=`UPDATE parcels set status=${newStatus} where id=${parcel_ID}`
 
+router.put('/api/v1/parcels/:parcelID/presentLocation', (req, res) =>{ //accessible by admins only
+	//change the status of a specific parcel order
+})
+let updateSpecificPresentlocQuery=`UPDATE parcels set presentLocation=${presentloc} where id=${parcel_ID}`
 
 module.exports= router
